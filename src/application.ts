46767d44cc44698d0aba9123ebdcec3d4d79821b
Cristopher Parrela <cristopherparrela@finchsolucoes.com.br>
2021-06-05 19:34:40 +0000

    feat: ativando novamente o restexplorer

--- src/application.ts
+++ src/application.ts
@@ -2,8 +2,10 @@ import {BootMixin} from '@loopback/boot';
 import {Application, ApplicationConfig} from '@loopback/core';
 import {RepositoryMixin} from '@loopback/repository';
 import {RestComponent, RestServer} from '@loopback/rest';
+import {RestExplorerBindings} from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
+import {RestExplorerComponent} from './components';
 import {MySequence} from './sequence';
 import {RabbitmqServer} from './servers';
 
@@ -21,6 +23,12 @@ export class MicroCatalogApplication extends BootMixin(
 
     const restServer = this.getSync<RestServer>('servers.RestServer');
     restServer.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
 
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
